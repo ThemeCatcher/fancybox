5cc4392d69452a8e9adff5764ebc0c134a44ed61
ThemeCatcher <ally@themecatcher.net>
2016-06-30 10:57:56 +0100

    Add gulp minify tasks and bower.json

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    cssnano = require('gulp-cssnano');
+
+gulp.task('js', function () {
+    return gulp.src('jquery.fancybox.js')
+               .pipe(rename({suffix: '.min'}))
+               .pipe(uglify({preserveComments: 'some'}))
+               .pipe(gulp.dest('.'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('jquery.fancybox.css')
+               .pipe(rename({suffix: '.min'}))
+               .pipe(cssnano())
+               .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['js', 'css']);
+
+gulp.task('watch', function () {
+    gulp.watch('jquery.fancybox.js', ['js']);
+    gulp.watch('jquery.fancybox.css', ['css']);
+});
