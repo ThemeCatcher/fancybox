2287e96d5ed46d2855769018eb155fd3558f0096
ThemeCatcher <ally@themecatcher.net>
2022-09-06 00:17:50 +0100

    Bump version to 1.3.9

--- jquery.fancybox.min.css
+++ jquery.fancybox.min.css
@@ -7,7 +7,7 @@
  * Copyright (c) 2008 - 2010 Janis Skarnelis
  * That said, it is hardly a one-person project. Many people have submitted bugs, code, and offered their advice freely. Their support is greatly appreciated.
  *
- * Version: 1.3.8 (16 Jun 2021)
+ * Version: 1.3.9 (06 Sep 2022)
  * Requires: jQuery v1.3+
  *
  * Dual licensed under the MIT and GPL licenses:
